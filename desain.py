749382c88fb6e544dd4b455e58a43570a50bcde6
Razan <razanaditya77@gmail.com>
2023-11-11 01:43:49 +0700

    update authentication

--- desain.py
+++ desain.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter, Request
+from fastapi.encoders import jsonable_encoder
 import json
 from pydantic import BaseModel
-
+import jwt
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.hash import bcrypt
 
 class Item(BaseModel):
 	id: int
@@ -19,23 +22,98 @@ class Konsul(BaseModel):
 	nama: str
 	nohp: int
 
+class User:
+    def __init__(self, id, username, password_hash):
+        self.id = id
+        self.username = username
+        self.password_hash = password_hash
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.password_hash)
+
 json_filename="desain.json"
 
-with open(json_filename,"r") as read_file:
-	data = json.load(read_file)
+with open(json_filename, "r") as read_file:
+    data = json.load(read_file)
 
 def write_data(data):
     with open(json_filename, "w") as write_file:
         json.dump(data, write_file, indent=4)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 app = FastAPI()
+JWT_SECRET = 'myjwtsecret'
+ALGORITHM = 'HS256'
 
-@app.get('/desain')
-async def read_desain():
+desain = APIRouter(tags=["Desain"], )
+konsuldesain = APIRouter(tags=["Konsul Desain"])
+alldesain = APIRouter(tags=["All Data"])
+authentication = APIRouter(tags=["Authentication"])
+
+def get_user_by_username(username):
+    for desain_user in data['user']:
+        if desain_user['username'] == username:
+            return desain_user
+    return None
+
+def authenticate_user(username: str, password: str):
+    user_data = get_user_by_username(username)
+    if not user_data:
+        return None
+
+    user = User(id=user_data['id'], username=user_data['username'], password_hash=user_data['password_hash'])
+
+    if not user.verify_password(password):
+        return None
+
+    return user
+
+@authentication.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        print(f"Invalid username or password for user: {form_data.username}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid username or password'
+        )
+
+    token = jwt.encode({'id': user.id, 'username': user.username}, JWT_SECRET, algorithm=ALGORITHM)
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = get_user_by_username(payload.get('username'))
+        return User(id=user['id'], username=user['username'], password_hash=user['password_hash'])
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
+@authentication.post('/users')
+async def create_user(username: str, password: str):
+    last_user_id = data['user'][-1]['id'] if data['user'] else 0
+    user_id = last_user_id + 1
+    user = jsonable_encoder(User(id=user_id, username=username, password_hash=bcrypt.hash(password)))
+    data['user'].append(user)
+    write_data(data)
+    return {'message': 'User created successfully'}
+
+@authentication.get('/users/me')
+async def get_user(user: User = Depends(get_current_user)):
+    return {'id': user.id, 'username': user.username, 'role': 'admin'}
+
+@desain.get('/desain')
+async def read_desain(user: User = Depends(get_current_user)):
 	return data['desain']
 
-@app.get('/desain/{status}')
-async def read_desain_status(status: str):
+@desain.get('/desain/{status}')
+async def read_desain_status(status: str, user: User = Depends(get_current_user)):
 	list_status = []
 	for desain_status in data['desain']:
 		if desain_status['status'] == status:
@@ -45,12 +123,12 @@ async def read_desain_status(status: str):
 		status_code=404, detail=f'desain not found'
 	)
 
-@app.get('/konsuldesain')
-async def read_konsuldesain():
+@konsuldesain.get('/konsuldesain')
+async def read_konsuldesain(user: User = Depends(get_current_user)):
 	return data['konsuldesain']
 
-@app.get('/')
-async def read_all():
+@alldesain.get('/alldata')
+async def read_all(user: User = Depends(get_current_user)):
     merged_data_list = []
 
     for desain_item in data['desain']:
@@ -75,8 +153,8 @@ async def read_all():
 
     return merged_data_list
 
-@app.get('/konsuldesain/{item_id}')
-async def read_konsuldesain(item_id: int):
+@konsuldesain.get('/konsuldesain/{item_id}')
+async def read_konsuldesain(item_id: int, user: User = Depends(get_current_user)):
 	for desain_item in data['konsuldesain']:
 		if desain_item['id_desainer'] == item_id:
 			return desain_item
@@ -84,8 +162,8 @@ async def read_konsuldesain(item_id: int):
 		status_code=404, detail=f'desainer not found'
 	)
 
-@app.post('/')
-async def add_items(items: dict):
+@alldesain.post('/alldata')
+async def add_items(items: dict, user: User = Depends(get_current_user)):
     konsul_dict = items.get('konsuldesain', {})
     item_dict = items.get('desain', {})
     permintaan_dict = items.get('permintaan', {})
@@ -127,8 +205,8 @@ async def add_items(items: dict):
 
     return "Add data successfully"
 
-@app.put('/desain/{item_id}')
-async def update_desain(item_id: int, item: Item):
+@desain.put('/desain/{item_id}')
+async def update_desain(item_id: int, item: Item, user: User = Depends(get_current_user)):
     item_dict = item.dict()
     item_found = False
     
@@ -155,8 +233,34 @@ async def update_desain(item_id: int, item: Item):
         status_code=404, detail=f'item not found'
     )
 
-@app.delete('/desain/{item_id}')
-async def delete_desain(item_id: int):
+@konsuldesain.put('/konsuldesain/{item_id}')
+async def update_desain(item_id: int, item: Konsul, user: User = Depends(get_current_user)):
+    item_dict = item.dict()
+    item_found = False
+    
+    for desain_idx, desain_item in enumerate(data['konsuldesain']):
+        if desain_item['id_desainer'] == item_id:
+            item_found = True
+            data['konsuldesain'][desain_idx] = {
+                "nama": item_dict["nama"],
+                "nohp": item_dict["nohp"],
+                "id_desainer": item_id
+            }
+            
+            with open(json_filename, "w") as write_file:
+                json.dump(data, write_file, indent=4)
+            
+            return "updated"
+    
+    if not item_found:
+        return "konsuldesain ID not found."
+    
+    raise HTTPException(
+        status_code=404, detail=f'item not found'
+    )
+
+@alldesain.delete('/alldata/{item_id}')
+async def delete_desain(item_id: int, user: User = Depends(get_current_user)):
     item_found = False
     
     # Inisialisasi list untuk menyimpan foreign keys
@@ -190,7 +294,7 @@ async def delete_desain(item_id: int):
             with open(json_filename,"w") as write_file:
                 json.dump(data, write_file, indent=4)
             
-            return "updated"
+            return "deleted"
     
     if not item_found:
         return "desain ID not found."
@@ -199,8 +303,8 @@ async def delete_desain(item_id: int):
         status_code=404, detail=f'item not found'
     )
 
-@app.put('/konsuldesain/{item_id}')
-async def update_konsuldesain(item_id: int, item: Konsul):
+@konsuldesain.put('/konsuldesain/{item_id}')
+async def update_konsuldesain(item_id: int, item: Konsul, user: User = Depends(get_current_user)):
     item_dict = item.dict()
     item_found = False
     
@@ -216,7 +320,7 @@ async def update_konsuldesain(item_id: int, item: Konsul):
             with open(json_filename, "w") as write_file:
                 json.dump(data, write_file, indent=4)
             
-            return "updated"
+            return "deleted"
     
     if not item_found:
         return "desain ID not found."
@@ -224,3 +328,12 @@ async def update_konsuldesain(item_id: int, item: Konsul):
     raise HTTPException(
         status_code=404, detail=f'item not found'
     )
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+app.include_router(authentication)
+app.include_router(desain)
+app.include_router(konsuldesain)
+app.include_router(alldesain)
